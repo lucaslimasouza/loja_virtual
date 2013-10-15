d756986adc51b073abf0e04ee894eb8afea64ffa
Lucas Lima de Souza <lucaslima4p@gmail.com>
2013-10-15 11:25:00 -0300

    adição do atributo preco classe livro

--- lib/livro.rb
+++ lib/livro.rb
@@ -2,10 +2,13 @@
 
 class Livro
 
-	def initialize(autor, isbn = "1", numero_de_paginas)
+	attr_accessor :preco
+
+	def initialize(autor, isbn = "1", numero_de_paginas, preco)
 		@autor = autor
 		@isbn = isbn
 		@numero_de_paginas = numero_de_paginas
+		@preco = preco
 	end
 
 	def to_s
